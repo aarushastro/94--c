0e6d8a64dc3260ea7df2ae07a500c0073f16155d
aarushastro <82552504+aarushastro@users.noreply.github.com>
2021-08-03 20:12:32 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD5yLf7J4cFwO7_QehmWQp14bx0mr1KfKs",
+    authDomain: "kwitter-project-b523d.firebaseapp.com",
+    projectId: "kwitter-project-b523d",
+    storageBucket: "kwitter-project-b523d.appspot.com",
+    messagingSenderId: "611849239503",
+    appId: "1:611849239503:web:d4d4db4e241289935270ba",
+    measurementId: "G-TVQJX4BBDB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
